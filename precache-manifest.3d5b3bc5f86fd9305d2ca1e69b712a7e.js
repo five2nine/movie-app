9ad9b462785258bd1c619f55335625245a72bc55
five2nine <fabre@pusan.ac.kr>
2018-10-20 13:43:07 +0900

    Updates

---
+++ precache-manifest.3d5b3bc5f86fd9305d2ca1e69b712a7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "d6d1ae226899a0fd53ae",
+    "url": "/movie_app/static/js/main.d6d1ae22.chunk.js"
+  },
+  {
+    "revision": "76fda7dd3b82a3a83c5c",
+    "url": "/movie_app/static/js/1.76fda7dd.chunk.js"
+  },
+  {
+    "revision": "d6d1ae226899a0fd53ae",
+    "url": "/movie_app/static/css/main.8d4afa15.chunk.css"
+  },
+  {
+    "revision": "976be3ceff7d9e266d2af3756e9a9ee1",
+    "url": "/movie_app/index.html"
+  }
+];
